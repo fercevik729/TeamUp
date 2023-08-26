24d403077c68fa6c55568555d72ea3f615336f63
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-08-25 23:53:38 -0700

    Added stereotype annotations

--- program-service/src/main/java/com/fercevik/programservice/repositories/SetRepository.java
+++ program-service/src/main/java/com/fercevik/programservice/repositories/SetRepository.java
@@ -3,11 +3,12 @@ package com.fercevik.programservice.repositories;
 import com.fercevik.programservice.models.Exercise;
 import com.fercevik.programservice.models.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SetRepository extends JpaRepository<Set, Long> {
     List<Set> findSetsByExercise(Exercise exercise);
 
-    List<>
 }
--- program-service/src/main/java/com/fercevik/programservice/repositories/WorkoutRepository.java
+++ program-service/src/main/java/com/fercevik/programservice/repositories/WorkoutRepository.java
@@ -3,9 +3,11 @@ package com.fercevik.programservice.repositories;
 import com.fercevik.programservice.models.Program;
 import com.fercevik.programservice.models.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
     List<Workout> findWorkoutsByProgram(Program program);
 
