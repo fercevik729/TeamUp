3af2314c63400fb80dca7069703cc0ad0e37f49b
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-08-28 13:01:19 -0700

    Started adding validations to DTO's

--- program-service/src/main/java/com/fercevik/programservice/dto/WorkoutDTO.java
+++ program-service/src/main/java/com/fercevik/programservice/dto/WorkoutDTO.java
@@ -1,5 +1,7 @@
 package com.fercevik.programservice.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Date;
@@ -8,8 +10,13 @@ import java.util.List;
 @Data
 public class WorkoutDTO {
     private Long workoutId;
+    @NotEmpty(message = "name cannot be empty")
+    @Size(min = 3, max = 50, message = "name must be between 3 and 50 characters long")
     private String name;
+    @Size(min = 3, max = 256, message = "description must be between 3 and 256 characters long")
     private String description;
     private Date date;
+
+    @NotEmpty(message = "exercises cannot be empty")
     private List<ExerciseDTO> exercises;
 }
