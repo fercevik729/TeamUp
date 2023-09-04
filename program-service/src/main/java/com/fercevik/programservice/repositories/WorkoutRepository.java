ecf546460179cc884c0c65851f68f366328a0f93
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-09-04 13:43:11 -0700

    Updated ExerciseRepositoryTests and created WorkoutRepositoryTests

--- program-service/src/main/java/com/fercevik/programservice/repositories/WorkoutRepository.java
+++ program-service/src/main/java/com/fercevik/programservice/repositories/WorkoutRepository.java
@@ -8,10 +8,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
     @Query("SELECT w.exercises FROM workouts w WHERE w.workoutId = :workoutId")
     List<Exercise> findExercisesForWorkout(Long workoutId);
 
+    @Query("SELECT w FROM workouts w WHERE w.description LIKE :desc")
+    List<Workout> findWorkoutsByDescription(String desc);
+
+    @Query("SELECT w FROM workouts w WHERE w.name LIKE :name")
+    Optional<Workout> findWorkoutByName(String name);
 }
