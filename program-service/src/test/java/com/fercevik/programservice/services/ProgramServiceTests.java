0d351e8feff18abd34e2c58a179b137d193c7a86
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-09-09 13:41:47 -0700

    Added more tests and annotations for repository tests

--- program-service/src/test/java/com/fercevik/programservice/services/ProgramServiceTests.java
+++ program-service/src/test/java/com/fercevik/programservice/services/ProgramServiceTests.java
@@ -7,7 +7,6 @@ import com.fercevik.programservice.exceptions.ProgramAlreadyExistsException;
 import com.fercevik.programservice.exceptions.ProgramNotFoundException;
 import com.fercevik.programservice.repositories.ProgramRepository;
 import com.fercevik.programservice.shared.DataConverter;
-import com.fercevik.programservice.utils.ConverterUtils;
 import com.fercevik.programservice.utils.RepoUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,7 +15,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.xml.crypto.Data;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -39,7 +37,7 @@ public class ProgramServiceTests {
     @Test
     void testGetUserPrograms() {
         UUID ownerId = UUID.randomUUID();
-        List<Program> data = List.of(ConverterUtils.createProgramDAO(ownerId));
+        List<Program> data = List.of(RepoUtils.createProgramDAO(ownerId));
 
         Mockito.when(repository.findProgramsByOwnerId(ownerId)).thenReturn(data);
         List<ProgramDTO> actual = programService.getUserPrograms(ownerId);
@@ -50,7 +48,7 @@ public class ProgramServiceTests {
     @Test
     void testGetActiveProgram() {
         UUID ownerId = UUID.randomUUID();
-        Program data = ConverterUtils.createProgramDAO(ownerId);
+        Program data = RepoUtils.createProgramDAO(ownerId);
 
         Mockito.when(repository.findProgramByActive(ownerId)).thenReturn(Optional.of(data));
         ProgramDTO actual = programService.getActiveProgram(ownerId);
@@ -61,7 +59,7 @@ public class ProgramServiceTests {
     @Test
     void testGetActiveProgramThrows() {
         UUID ownerId = UUID.randomUUID();
-        Program data = ConverterUtils.createProgramDAO(ownerId);
+        Program data = RepoUtils.createProgramDAO(ownerId);
         data.setActive(false);
 
         Mockito.when(repository.findProgramByActive(ownerId)).thenReturn(Optional.empty());
@@ -71,7 +69,7 @@ public class ProgramServiceTests {
     @Test
     void testGetProgram() {
         UUID ownerId = UUID.randomUUID();
-        Program data = ConverterUtils.createProgramDAO(ownerId);
+        Program data = RepoUtils.createProgramDAO(ownerId);
         Long id = data.getProgramId();
 
         Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, id)).thenReturn(Optional.of(data));
@@ -83,11 +81,11 @@ public class ProgramServiceTests {
     @Test
     void testGetProgramThrows() {
         UUID ownerId = UUID.randomUUID();
-        Program data = ConverterUtils.createProgramDAO(ownerId);
+        Program data = RepoUtils.createProgramDAO(ownerId);
         long id = data.getProgramId();
 
-        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, id+1)).thenReturn(Optional.empty());
-        assertThrows(ProgramNotFoundException.class, () -> programService.getProgram(ownerId, id+1));
+        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, id + 1)).thenReturn(Optional.empty());
+        assertThrows(ProgramNotFoundException.class, () -> programService.getProgram(ownerId, id + 1));
     }
 
     @Test
@@ -101,10 +99,11 @@ public class ProgramServiceTests {
     @Test
     void testCreateProgram() {
         UUID ownerId = UUID.randomUUID();
-        ProgramDTO data = ConverterUtils.createProgramDTO();
+        ProgramDTO data = RepoUtils.createProgramDTO();
         Program program = DataConverter.convertProgramFromDTO(ownerId, data);
 
-        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, data.getProgramId())).thenReturn(Optional.empty());
+        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, data.getProgramId()))
+                .thenReturn(Optional.empty());
         Mockito.when(repository.findProgramByName(ownerId, data.getName())).thenReturn(Optional.empty());
         Mockito.when(repository.save(program)).thenReturn(program);
         long actual = programService.createProgram(ownerId, data);
@@ -114,18 +113,42 @@ public class ProgramServiceTests {
     @Test
     void testCreateProgramSameIdExists() {
         UUID ownerId = UUID.randomUUID();
-        ProgramDTO data = ConverterUtils.createProgramDTO();
+        ProgramDTO data = RepoUtils.createProgramDTO();
         Program program = DataConverter.convertProgramFromDTO(ownerId, data);
-        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, data.getProgramId())).thenReturn(Optional.of(program));
+        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, data.getProgramId()))
+                .thenReturn(Optional.of(program));
         assertThrows(ProgramAlreadyExistsException.class, () -> programService.createProgram(ownerId, data));
     }
+
     @Test
     void testCreateProgramSameNameExists() {
         UUID ownerId = UUID.randomUUID();
-        ProgramDTO data = ConverterUtils.createProgramDTO();
+        ProgramDTO data = RepoUtils.createProgramDTO();
         Program program = DataConverter.convertProgramFromDTO(ownerId, data);
-        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, data.getProgramId())).thenReturn(Optional.empty());
+        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, data.getProgramId()))
+                .thenReturn(Optional.empty());
         Mockito.when(repository.findProgramByName(ownerId, data.getName())).thenReturn(Optional.of(program));
         assertThrows(ProgramAlreadyExistsException.class, () -> programService.createProgram(ownerId, data));
+    }
+
+    @Test
+    void testUpdateProgram() {
+        UUID ownerId = UUID.randomUUID();
+        ProgramDTO data = RepoUtils.createProgramDTO();
+        Program program = DataConverter.convertProgramFromDTO(ownerId, data);
+        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, program.getProgramId()))
+                .thenReturn(Optional.of(program));
+        Mockito.when(repository.save(program)).thenReturn(program);
+        assertEquals(program.getProgramId(), programService.updateProgram(ownerId, data));
+    }
+
+    @Test
+    void testUpdateProgramDoesntExist() {
+        UUID ownerId = UUID.randomUUID();
+        ProgramDTO data = RepoUtils.createProgramDTO();
+        Program program = DataConverter.convertProgramFromDTO(ownerId, data);
+        Mockito.when(repository.findProgramByOwnerIdAndProgramId(ownerId, program.getProgramId()))
+                .thenReturn(Optional.empty());
+        assertThrows(ProgramNotFoundException.class, () -> programService.updateProgram(ownerId, data));
     }
 }
--- program-service/src/test/java/com/fercevik/programservice/utils/RepoUtils.java
+++ program-service/src/test/java/com/fercevik/programservice/utils/RepoUtils.java
@@ -2,28 +2,57 @@ package com.fercevik.programservice.utils;
 
 import com.fercevik.programservice.dao.Exercise;
 import com.fercevik.programservice.dao.Program;
+import com.fercevik.programservice.dao.Set;
 import com.fercevik.programservice.dao.Workout;
+import com.fercevik.programservice.dto.ExerciseDTO;
+import com.fercevik.programservice.dto.ProgramDTO;
+import com.fercevik.programservice.dto.SetDTO;
+import com.fercevik.programservice.dto.WorkoutDTO;
+import com.fercevik.programservice.shared.WeightUnits;
 
+import java.time.Duration;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class RepoUtils {
-    public static Program createProgram() {
-        UUID ownerId = UUID.randomUUID();
-        List<String> tags = new ArrayList<>();
-        tags.add("Bodybuilding");
-        tags.add("Powerlifting");
-        return Program.builder().tags(tags).active(true).ownerId(ownerId).name("First Program").build();
+    public static SetDTO createSetDTO() {
+        return SetDTO.builder().setId(1).duration(Duration.ofSeconds(20)).weight(15).reps(12).build();
     }
 
-    public static Workout createWorkout() {
-        return Workout.builder().description("Leg day! :)").name("Monday").date(LocalDate.now()).build();
+    public static Set createSetDAO() {
+        return Set.builder().setId(1).duration(Duration.ofSeconds(20)).weight(15).reps(12).build();
     }
 
-    public static Exercise createExercise() {
-        return Exercise.builder().description("Biceps-focused and in the shortened position").target("Biceps")
-                .name("Preacher Curls").build();
+    public static ExerciseDTO createExerciseDTO() {
+        return ExerciseDTO.builder().exerciseId(1).name("Preacher Curls").description("Biceps-focused and in the shortened position")
+                .target("Biceps").sets(List.of(RepoUtils.createSetDTO())).build();
+    }
+
+    public static Exercise createExerciseDAO() {
+        return Exercise.builder().exerciseId(1).name("Preacher Curls").description("Biceps-focused and in the shortened position")
+                .target("Biceps").sets(List.of(RepoUtils.createSetDAO())).build();
+    }
+
+    public static WorkoutDTO createWorkoutDTO() {
+        return WorkoutDTO.builder().workoutId(1).date(LocalDate.now()).exercises(List.of(createExerciseDTO())).name("Monday")
+                .description("Leg Day! :)").build();
+    }
+
+    public static Workout createWorkoutDAO() {
+        return Workout.builder().workoutId(1).date(LocalDate.now()).exercises(List.of(createExerciseDAO())).name("Monday")
+                .description("Leg Day! :)").build();
+    }
+
+    public static ProgramDTO createProgramDTO() {
+        return ProgramDTO.builder().programId(1).active(true).tags(List.of("Bodybuilding", "Fitness", "Beginner"))
+                .units(WeightUnits.KILOGRAMS).name("Program for Beginner Clients")
+                .workouts(List.of(RepoUtils.createWorkoutDTO())).build();
+    }
+
+    public static Program createProgramDAO(UUID ownerId) {
+        return Program.builder().programId(1).active(true).tags(List.of("Bodybuilding", "Fitness", "Beginner"))
+                .ownerId(ownerId).workouts(List.of(RepoUtils.createWorkoutDAO())).units(WeightUnits.KILOGRAMS)
+                .name("Program for Beginner Clients").build();
     }
 }
